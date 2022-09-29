06b1e76fd2b2f796e352ce6b9bdb9b82343784eb
Juan Fernando Brambila Rivera <72884006+Jen412@users.noreply.github.com>
2022-09-29 09:22:02 -0500

    Cambios 29/09/22

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import Layout from './components/Layout'
+import NuevoCliente ,{action as nuevoClienteAction}from './pages/NuevoCliente'
+import Index, {loader as clientesLoader} from './pages/Index'
+import ErrorPage from './components/ErrorPage'
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout />,
+        children: [
+            {
+                index: true,
+                element: <Index/>,
+                loader: clientesLoader,
+                errorElement: <ErrorPage/>
+            },
+            {
+                path: "/clientes/nuevo",
+                element: <NuevoCliente/>,
+                action: nuevoClienteAction
+            }
+        ]
+    }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <RouterProvider router={router}/> 
+    </React.StrictMode>
 )
